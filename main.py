efb7a8d6eafefd71e140ee4b976ef7e9fee8901f
c314 <47485054+camden314@users.noreply.github.com>
2019-02-09 14:47:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ bot = commands.Bot(command_prefix='$')
 async def test(ctx):
     await ctx.send("something")
 threading.Thread(target=flask_server.run).start()
-bot.run("TOKEN_HERE")
+bot.run("NTQxNzgzOTM5MTc4NTYxNTM2.D0DNnA.bZPaQyRLp9Zl-A9-7Kl89SQSbTM")
